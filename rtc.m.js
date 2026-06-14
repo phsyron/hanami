0efe8eaffb259eaf375a53e73a4b0cef2a99c631
Benjamin Martin <2959073+phsyron@users.noreply.github.com>
2026-06-13 17:31:59 -0700

    bugfixes and basic host/join form

--- rtc.m.js
+++ rtc.m.js
@@ -1,17 +1,20 @@
 
 export var freeStun = {iceServers: [{urls: 'stun:stun1.l.google.com:19302'}]};
 
-export class RTC {
+export class RTC extends EventTarget{
 
     channel;
     configuration;
+    logging;
 
-    constructor(configuration){
+    constructor(configuration,logging=true){
+        super();
         if(configuration) {
             this.configuration = configuration;
         } else{
           this.configuration = {};
         }
+        this.logging = logging;
         this.connection = new RTCPeerConnection(this.configuration);
         this.connection.onicecandidate = (x)=>{this.onIceCandidate(x)};
         this.connection.ondatachannel = (x)=>{this.onDataChannel(x)};
@@ -22,43 +25,62 @@ export class RTC {
         this.channel.onopen = (x) => this.onOpen(x);
         this.channel.onclose = (x) => this.channelClose(x);
     }
-    
+
     onIceCandidate(e){
-        console.log('new ICE candidate',JSON.stringify(this.connection.localDescription));
+        let d = this.connection.localDescription;
+        if(this.logging){
+            console.log('new ICE candidate',JSON.stringify(d));
+        }
+        this.dispatchEvent(new IceCandidateEvent(d));
     }
 
     onMessage(e){
-        console.log('new message:', e.data);
+        if(this.logging){
+            console.log('new message:', e.data);
+        }
+        this.dispatchEvent(new MessageEvent(e.data));
     }
 
     onOpen(e){
-        console.log('channel open');
+        if(this.logging){
+            console.log('channel open');
+        }
+        this.dispatchEvent(new OpenEvent());
     }
 
     onClose(e){
-        console.log('channel closed');
+        if(this.logging){
+            console.log('channel closed');
+        }
+        this.dispatchEvent(new CloseEvent());
     }
-    
+
     onDataChannel(e){
-        console.log('new data channel',e);
+        if(this.logging){
+            console.log('new data channel',e);
+        }
+        this.dispatchEvent(new DataChannelEvent());
         this.channel = e.channel;
         this.setChannelCallbacks();
     }
 
     onAnswerCreated(answer){
-        let a = 
-        console.log('answer created:',JSON.stringify(this.connection.localDescription));
+        let d = this.connection.localDescription;
+        if(this.logging){
+            console.log('answer created:',JSON.stringify(d));
+        }
+        this.dispatchEvent(new AnswerCreatedEvent(d));
     }
 
     one(){
         this.channel = this.connection.createDataChannel('data');
-        console.log('foo');
         this.setChannelCallbacks();
         this.connection.createOffer().then( (o) => this.connection.setLocalDescription(o))
     }
 
     two(offer){
-        this.connection.setRemoteDescription(offer).then(console.log('done'));
+        this.connection.setRemoteDescription(offer)
+            .then(()=>{if(this.logging){console.log('done')}});
         this.connection.createAnswer()
             .then(a=>this.connection.setLocalDescription(a))
             .then(a=>this.onAnswerCreated(a));
@@ -66,6 +88,76 @@ export class RTC {
 
     three(answer){
         this.connection.setRemoteDescription(answer)
-            .then(console.log('done'));
+            .then(()=>{if(this.logging){console.log('done')}});
     }
 }
+
+class IceCandidateEvent extends Event {
+
+    #description;
+
+    constructor(description){
+        super('icecandidate');
+        this.#description = description;
+    }
+
+    get description(){
+        return this.#description;
+    }
+
+}
+
+class MessageEvent extends Event {
+
+    #message;
+
+    constructor(message){
+        super('message');
+        this.#message = message;
+    }
+
+    get message(){
+        return this.#message;
+    }
+
+}
+
+class OpenEvent extends Event {
+
+    constructor(){
+        super('open');
+    }
+
+}
+
+class CloseEvent extends Event {
+
+    constructor(){
+        super('close');
+    }
+
+}
+
+class DataChannelEvent extends Event {
+
+    constructor(){
+        super('datachannel');
+    }
+
+}
+
+class AnswerCreatedEvent extends Event {
+
+    #description;
+
+    constructor(description){
+        super('answercreated');
+        this.#description = description;
+    }
+
+    get description(){
+        return this.#description;
+    }
+
+}
+
--- script.m.js
+++ script.m.js
@@ -18,41 +18,67 @@ worker.onmessage = (msg) => {
     }
 };
 
-let form = document.querySelector('form');
-let output = form.querySelector('output');
-let ta = form.querySelector('textarea');
-ta.value="";
-let butt_copy = form.querySelector('button[name="copy"]');
-butt_copy.onclick = () => {
-    navigator.clipboard.write([new ClipboardItem({["text/plain"]:output.value})]);
-};
-let butt_start = form.querySelector('button[name="start"]');
-butt_start.onclick = () => worker.postMessage({type:'newGame',payload:{nPlayers:2}});
+let gtos = document.querySelectorAll('.gametypeoptions');
+let show_gto = (gt) => {
+    console.log(gt);
+    for(let gto of gtos){
+        if(gto.id == gt){
+            gto.removeAttribute('disabled');
+        } else {
+            gto.setAttribute('disabled','');
+        }
+    }
+}
+
+for(let el of document.querySelectorAll('input[name="gametype"]')){
+    el.oninput = (e) => show_gto(e.target.value);
+}
+
+show_gto(document.querySelector('input[name="gametype"][checked]').value);
 
 class myRTC extends RTC{
     
-    onIceCandidate(e){
-        output.value = btoa(JSON.stringify(this.connection.localDescription));
-    }
-
     setChannelCallbacks(e){
-        document.worker.postMessage({type:'channel',payload:theRTC.channel},[theRTC.channel]);
+        document.worker.postMessage({type:'channel',payload:this.channel},[this.channel]);
     }
 
 }
 
-
-
-let theRTC = new myRTC(freeStun);
-window.theRTC = theRTC;
-
+let form = document.querySelector('form');
+let houtput = form.querySelector('fieldset#host output');
+let hta = form.querySelector('fieldset#host textarea');
+let hbutt_copy = form.querySelector('button[name="copy"]');
+hbutt_copy.onclick = () => {
+    navigator.clipboard.write([new ClipboardItem({["text/plain"]:houtput.value})]);
+};
+let hbutt_start = form.querySelector('button[name="start"]');
+hbutt_start.onclick = () => worker.postMessage({type:'newGame',payload:{nPlayers:2}});
 let butt_host = form.querySelector('button[name="host"]');
 butt_host.onclick = (e) => {
     theRTC.one();
 }
+
+let theRTC = new myRTC(freeStun,true);
+theRTC.addEventListener('icecandidate',(e)=>{
+    console.log(JSON.stringify(e.description));
+    houtput.value = btoa(JSON.stringify(e.description));
+});
+theRTC.addEventListener('answercreated',(e)=>{
+    console.log(JSON.stringify(e.description));
+    joutput.value = btoa(JSON.stringify(e.description));
+});
+window.theRTC = theRTC;
+
+let joutput = form.querySelector('fieldset#join output');
+let jta = form.querySelector('fieldset#join textarea');
+let jbutt_copy = form.querySelector('fieldset#join button[name="copy"]');
+jbutt_copy.onclick = () => {
+    navigator.clipboard.write([new ClipboardItem({["text/plain"]:joutput.value})]);
+};
+
 let butt_join = form.querySelector('button[name="join"]');
-butt_join.onclick = (e) => theRTC.two(JSON.parse(atob(ta.value)));
+butt_join.onclick = (e) => theRTC.two(JSON.parse(atob(jta.value)));
 let butt_connect = form.querySelector('button[name="connect"]');
-butt_connect.onclick = (e) => theRTC.three(JSON.parse(atob(ta.value)));
+butt_connect.onclick = (e) => theRTC.three(JSON.parse(atob(hta.value)));
 
 
